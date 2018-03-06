b645fdfd22804a90aaa1f5fd180bccab3216605f
Aspen James <aspenjames@tqca.org>
2018-03-06 21:48:42 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,6 +2,19 @@ class School
   attr_reader :name
   
   def initialize(name)
-    @name = name  
+    @name = name
+  end
+
+  def roster
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name
+  end
+
+  def grade(n)
+    @roster[n]
   end
 end 
